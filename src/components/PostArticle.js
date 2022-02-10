2bc2ef3f2b7f84d65c44a574eb017fcfe7ab49eb
Philip Burgess <philipgrahamburgess@gmail.com>
2022-02-10 22:12:25 +0000

    create post textarea added

--- src/components/PostArticle.js
+++ src/components/PostArticle.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export function PostArticle() {
   const [modalClass, setModalClass] = useState("hidden-modal");
+  const [input, setInput] = useState("");
 
   const showNewPostForm = () => {
     if (modalClass === "modal") setModalClass("hidden-modal");
@@ -12,8 +13,21 @@ export function PostArticle() {
   return (
     <div>
       <div className={modalClass + " fade-in"}>
-        <label>New post popup</label>
-        <button onClick={showNewPostForm}>close</button>
+        <label></label>
+        <h2>Title</h2>
+        <h2>topic</h2>
+        <textarea
+          className="post-textarea"
+          name="post article"
+          placeholder="Article Text"
+          spellCheck={true}
+          value={input}
+          rows={20}
+          maxLength={1000}
+          onChange={(event) => setInput(event.target.value)}
+          wrap="hard"
+        />
+        <button onClick={showNewPostForm}>Submit</button>
       </div>
       <div className="PostArticle">
         <input
