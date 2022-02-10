16ec4ea0129da0a5edbfa638e888177b535a8325
Philip Burgess <philipgrahamburgess@gmail.com>
2022-02-10 21:54:13 +0000

    create post modal popup with animation added

--- src/components/PostArticle.js
+++ src/components/PostArticle.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 export function PostArticle() {
+  const [modalClass, setModalClass] = useState("hidden-modal");
+
+  const showNewPostForm = () => {
+    if (modalClass === "modal") setModalClass("hidden-modal");
+    else setModalClass("modal");
+    console.log(modalClass);
+  };
+
   return (
-    <div className="PostArticle">
-      <h2>Post Article</h2>
+    <div>
+      <div className={modalClass + " fade-in"}>
+        <label>New post popup</label>
+        <button onClick={showNewPostForm}>close</button>
+      </div>
+      <div className="PostArticle">
+        <input
+          className="newpost-box"
+          placeholder="Create Post"
+          onFocus={showNewPostForm}
+        />
+      </div>
     </div>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,19 @@
+import { useContext } from "react";
+import { UserContext } from "../contexts/User";
+
 import { useState } from "react";
+
 import { Articles } from "../components/Articles";
-// import { PostArticle } from "../components/PostArticle";
+import { PostArticle } from "../components/PostArticle";
 import { Sort } from "../components/Sort";
 
 export function Home() {
+  const { user } = useContext(UserContext);
   const [sort, setSort] = useState(`?sort_by=created_at`);
 
   return (
     <div className="Home">
-      {/* <PostArticle /> */}
+      {user ? <PostArticle /> : <></>}
       <Sort setSort={setSort} />
       <Articles query={sort} />
     </div>
